29f1d1b20e2eab562c7773afe4ab4d02b3ba38f7
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2023-02-22 19:41:59 +0200

    add slider swiper

---
+++ src/index.js
@@ -0,0 +1,43 @@
+new Swiper('.swiper', {  
+  initialSlide: 0,
+  slidesPerView: "auto",
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  spaceBetween: 15,
+  mousewheel: {
+    sensitivity: 1,
+    eventsTarget: ".swiper"
+  },
+  watchOverflow: true,
+  initialSlide: 1,
+  breakpoints: {
+    360: {
+      sliderPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    },
+    480: {
+      sliderPerView: 2,
+    },
+    768: {
+      sliderPerView: 3,
+    },
+    1024: {
+      sliderPerView: 4,
+
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+      mousewheel: {
+        sensitivity: 4,
+        eventsTarget: ".swiper"
+      },
+    }
+  }
+})
